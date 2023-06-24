5d07c25c072a61a33956be6e3ae4fd1fabb4a29a
abdullahamdy <abdullahhamdy29@gmail.com>
2023-06-24 14:00:14 +0200

    install laravel passport

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('login', [UserController::class,'login']);
+Route::post('register', [UserController::class,'register']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -23,3 +25,7 @@ Route::apiResource('posts',PostController::class);
 Route::apiResource('categories',CategoryController::class);
 Route::get('category/{slug}/posts',[PostController::class,'categoryPost']);
 Route::get('getpost/{query}',[PostController::class,'getPost']);
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', [UserController::class,'details']);
+
+});
