f5909c321254706711717508b155d6a7d1a976aa
abdullahamdy <abdullahhamdy29@gmail.com>
2023-10-09 16:02:18 +0300

    make newComment event and channel

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Events\NewComment;
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
 use Illuminate\Http\Request;
@@ -19,6 +20,8 @@ class CommentController extends Controller
             'user_id'=>Auth::id(),
             'post_id'=>$request->post_id,
         ]);
+
+        broadcast(new NewComment($comment->user,$comment))->toOthers();
         return response()->json(['id'=>$comment->id,'body'=>$comment->body,'user'=>$comment->user,'added_at'=>$comment->created_at->diffForHumans()]);
     }
 }
